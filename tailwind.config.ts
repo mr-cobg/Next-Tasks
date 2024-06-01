0914f9e5e82a8ddbde34709351002445338375a6
Utsho de Bravestone <utshodebravestone@gmail.com>
2024-06-01 23:38:32 +0600

    added theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: ["light", "dark", "coffee"],
+  },
+  plugins: [require("daisyui")],
 };
 export default config;
